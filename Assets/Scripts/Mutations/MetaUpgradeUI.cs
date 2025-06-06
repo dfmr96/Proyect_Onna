326ee4906885e2b7247f14cfabf9c1db62d2278e
David Marcano <dfmr96@gmail.com>
2025-06-06 14:07:14 -0300

    feat(ui): add MetaUpgradeUI for applying permanent stat upgrades in hub
    Created a UI component that allows testing and applying flat and percent-based upgrades to MetaStatBlock from the hub. Useful for verifying metaprogression systems.

---
+++ Assets/Scripts/Mutations/MetaUpgradeUI.cs
@@ -0,0 +1,44 @@
+﻿using Player;
+using Player.Stats;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MetaUpgradeUI : MonoBehaviour
+    {
+        [SerializeField] private PlayerModel player;
+        [SerializeField] private StatReferences statRefs;
+
+        [Header("Botones")]
+        [SerializeField] private Button speedButton;
+        [SerializeField] private Button healthButton;
+        [SerializeField] private Button damageButton;
+
+        private void Start()
+        {
+            if (player == null) player = FindObjectOfType<PlayerModel>();
+
+            speedButton.onClick.AddListener(() => ApplyFlat(statRefs.movementSpeed, 1f));
+            healthButton.onClick.AddListener(() => ApplyFlat(statRefs.maxVitalTime, 5f));
+            damageButton.onClick.AddListener(() => ApplyPercent(statRefs.damage, 10f));
+        }
+
+        private void ApplyFlat(StatDefinition stat, float amount)
+        {
+            player.StatContext.Meta?.AddFlatBonus(stat, amount);
+        }
+
+        private void ApplyPercent(StatDefinition stat, float percent)
+        {
+            player.StatContext.Meta?.AddPercentBonus(stat, percent);
+        }
+        
+        public void ResetMetaStats()
+        {
+            MetaStatSaveSystem.DeleteSaveFile();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
