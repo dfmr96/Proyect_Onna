17d3c2511ced423af2fd37cc28f648f812ac32cd
David Marcano <dfmr96@gmail.com>
2025-06-06 14:08:04 -0300

    feat(scene): add SceneTransitionUI for switching between Hub and Room scenes
    Implemented SceneTransitionUI to handle scene loading and GameMode assignment. Includes Save call for meta stats before entering the run.

---
+++ Assets/Scripts/Player/SceneTransitionUI.cs
@@ -0,0 +1,25 @@
+﻿using Player;
+using Player.Stats;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionUI : MonoBehaviour
+{
+    public void StartRun()
+    {
+        var bootstrapper = FindObjectOfType<PlayerModelBootstrapper>();
+        var metaStats = bootstrapper.MetaStats;
+        var registry = bootstrapper.Registry; 
+        MetaStatSaveSystem.Save(metaStats, registry);
+        GameModeSelector.SelectedMode = GameMode.Run;
+        SceneManager.LoadScene("RoomScene");
+    }
+    
+    public void ReturnToHub()
+    {
+        GameModeSelector.SelectedMode = GameMode.Hub;
+        SceneManager.LoadScene("HubTestScene");
+    }
+    
+    
+}
