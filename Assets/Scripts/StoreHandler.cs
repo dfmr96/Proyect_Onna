9d327f8129c16d6e31892e4d3643eadc6c035153
alexnoire89 <paszczuk.a@gmail.com>
2025-12-06 14:22:06 -0300

    +Fix Tienda:
    Ahora cuando se abre la tienda se encuentra seleccionado por default la primera opcion

--- Assets/Scripts/StoreHandler.cs
+++ Assets/Scripts/StoreHandler.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class StoreHandler : MonoBehaviour
 {
@@ -101,6 +102,20 @@ public class StoreHandler : MonoBehaviour
 
             var uiButton = buttonObj.GetComponent<Button>();
             //uiButton.interactable = true;
+        }
+
+        //Seleccionar automaticamente el primer item
+        if (upgradeButtons.Count > 0)
+        {
+            var firstButtonObj = upgradeButtons[0];
+            var uiButton = firstButtonObj.GetComponent<Button>();
+
+            // Esto activa el highlight normal del botón (según su configuración original)
+            uiButton.Select();
+
+            // Opcionalmente también podés disparar el detalle:
+            var b = firstButtonObj.GetComponent<BuyUpgradeButton>();
+            OnUpgradeClicked(b);
         }
     }
 
